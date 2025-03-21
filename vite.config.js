5c2cea78a89495c87ebd18b72de45b1010d1d68c
Abel Enrique Valdivia Daza <132954955+vdabel@users.noreply.github.com>
2025-03-21 16:06:00 -0500

    update project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/card-react/',
+  base: 'https://vdabel.github.io/card-react',
 })
